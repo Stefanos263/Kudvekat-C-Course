5c2d5495d663eada84df33347c997b86a00c08f2
Stefanos263 <stefanoskempas26@gmail.com>
2023-11-22 20:51:13 +0200

    another commit

--- Program.cs
+++ Program.cs
@@ -10,7 +10,29 @@ namespace kudvekat1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello stef");
+            Console.WriteLine("Please enter your firstname :");
+           String UserName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your lastname :");
+            String LastName = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}, {1}", UserName,LastName);
+            //Console.WriteLine("hello " + UserName);
+
+            bool? AreYouAMajor = null;
+
+            if (AreYouAMajor == true)
+            {
+                Console.WriteLine("user is a major");
+            }
+            else if (AreYouAMajor == false)
+            {
+                Console.WriteLine("user is not a major");
+            }
+            else
+            {
+                Console.WriteLine("user did not answer the question");
+            }
         }
     }
 }
